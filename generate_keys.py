49b6579fe9abe316cabd07233ec0b0bc693341dd
traial-afk <nithinsripathi93@gmail.com>
2024-11-23 02:35:44 -0500

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,14 +3,15 @@ from pathlib import Path
 import streamlit_authenticator as stauth
 
 names = ["snehit", "vaddi", "test"]
-usernames = ["snehit", "vaddi", "test"]
+usernames = ["snehit", "vaddi", "Test"]
 passwords = ["snehit", "snehit", "test"]
 
-# Use generate_hashed_passwords() instead of generate()
-hashed_passwords = stauth.Hasher(passwords).generate_hashed_passwords()
+# Hash the passwords
+hashed_passwords = stauth.Hasher(passwords).hashed_passwords
 
+# Save the hashed passwords to a file
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("wb") as file:
     pickle.dump(hashed_passwords, file)
 
-print("File 'hashed_pw.pkl' generated successfully!")
+print("Hashed passwords saved successfully!")
